abff50b9bd4a04732611c56750fe9226c9e4dec8
Adrian Ławrentjew <adrian.lawrentjew@gmail.com>
2018-10-06 14:52:33 +0200

    Add jackson-databind

--- src/main/java/pl/coderslab/warsztat5katw01/model/Book.java
+++ src/main/java/pl/coderslab/warsztat5katw01/model/Book.java
@@ -8,7 +8,8 @@ public class Book {
     private String publisher;
     private String type;
 
-    public Book(String isbn, String title, String author, String publisher, String type) {
+    public Book(long id, String isbn, String title, String author, String publisher, String type) {
+        this.id = id;
         this.isbn = isbn;
         this.title = title;
         this.author = author;
