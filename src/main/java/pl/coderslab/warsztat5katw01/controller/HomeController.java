74303fa483444782cecbb129da6cf468310ee1ec
Adrian Ławrentjew <adrian.lawrentjew@gmail.com>
2018-10-06 14:03:25 +0200

    Spring initialize

---
+++ src/main/java/pl/coderslab/warsztat5katw01/controller/HomeController.java
@@ -0,0 +1,14 @@
+package pl.coderslab.warsztat5katw01.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    @ResponseBody
+    public String home() {
+        return "You are at home!";
+    }
+}
