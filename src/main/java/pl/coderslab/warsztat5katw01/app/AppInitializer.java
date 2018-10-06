891bda642c50545e9555be7c7bbaf2548b5e36c7
Adrian Ławrentjew <adrian.lawrentjew@gmail.com>
2018-10-06 14:31:59 +0200

    Add UFT-8

--- src/main/java/pl/coderslab/warsztat5katw01/app/AppInitializer.java
+++ src/main/java/pl/coderslab/warsztat5katw01/app/AppInitializer.java
@@ -1,21 +1,32 @@
 package pl.coderslab.warsztat5katw01.app;
 
-import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.servlet.DispatcherServlet;
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRegistration;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
-public class AppInitializer implements WebApplicationInitializer {
-    public void onStartup(ServletContext container) throws ServletException {
-        AnnotationConfigWebApplicationContext ctx =
-                new AnnotationConfigWebApplicationContext();
-        ctx.register(AppConfig.class);
-        ctx.setServletContext(container);
-        ServletRegistration.Dynamic servlet =
-                container.addServlet("dispatcher", new DispatcherServlet(ctx));
-        servlet.setLoadOnStartup(1);
-        servlet.addMapping("/");
+import javax.servlet.Filter;
+
+
+public	class	AppInitializer	extends
+        AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected	Class<?>[]	getRootConfigClasses()	{
+        return	null;
+    }
+
+    @Override
+    protected	Class<?>[]	getServletConfigClasses()	{
+        return	new	Class[]{AppConfig.class};		}
+
+    @Override
+    protected	String[]	getServletMappings()	{
+        return	new	String[]{"/"};
+    }
+
+    @Override
+    protected	Filter[]	getServletFilters()	{
+        CharacterEncodingFilter characterEncodingFilter	=
+                new	CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        return	new	Filter[]	{	characterEncodingFilter	};
     }
 }
